0664d36f9ca9eeb043893d41f0967a65d6113da6
John Yeo <johnyeocx@gmail.com>
2024-02-27 22:33:31 +0000

    initial websocket server set up

--- client_server_interface/server/server2.js
+++ client_server_interface/server/server2.js
@@ -1,18 +1,49 @@
-import {WebSocketServer} from 'ws'
+import { WebSocketServer } from 'ws';
+import { connectToDB } from './db.js'; // Assuming you have a separate file for DB connection
+import { config } from 'dotenv';
 
-const sockserver = new WebSocketServer({port: 8080})
+// Initialize DB connection
+const main = async() => {
+    config();
+    await connectToDB();
 
-sockserver.on('connection', (socket) => {
-    console.log('connected')
-    socket.on('message', (data) => {
-        console.log(data)
-        socket.send(`Received datastream: ${data}`)
-    })
-    socket.on('close', () => {
-        console.log('closed')
-    })
-    socket.on('error', (err) => {
-        console.log(err)
-    })
-})
+    const ws = new WebSocketServer({port: 8080})
 
+    ws.on('listening', () => {
+        console.log('listening to ws connections on port 8080')
+    })
+
+    ws.on('connection', (socket) => {
+        
+        
+        socket.on('message', (data) => {
+            const utf8Data = data.toString('utf8');
+            try {
+                const jsonData = JSON.parse(utf8Data);
+                console.log("Received: ", jsonData)
+            } catch (error) {
+                console.error('Error parsing JSON:', error);
+            }
+
+            
+            socket.send(`Received message!`)
+
+            setInterval(() => {
+                socket.send('Message from server');
+            }, 5000);
+            
+        })
+
+
+        socket.on('close', () => {
+            console.log('closed')
+        })
+
+
+        socket.on('error', (err) => {
+            console.log(err)
+        })
+    })
+}
+
+main();
