929f574c5cb3ce550312515ed19fe80e67629048
kr1pt0 <fedorrrd@gmail.com>
2024-03-03 03:28:50 +0000

    camera follow + zoom

--- game/Assets/scripts/CamerFollow.cs
+++ game/Assets/scripts/CamerFollow.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class CamerFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //cam issue
+    Camera cam;
+    public Vector3 Offset;
+    public Vector3 Change;
+    public float Speed = 0.4f;
+    //zoming variables
+    public float MaxZoom = 100f;
+    public float MinZoom = 5f;
+    public float ZoomSpeed = 1f;
+    //debugging tool
+    public float ZoomController = 1f;
+
+    
+    
+    private void Start()
     {
-        
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        Zoom();
+    }
+
+    public void Zoom()
+    {
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, GetZoom(), ZoomSpeed);
+
+        cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, MinZoom, MaxZoom);
+    }
+
+    public float GetZoom()
+    {
+        MassSpawner ms = MassSpawner.ins;
+
+        Bounds bounds = new Bounds(ms.Players[0].transform.position, Vector3.zero);
+        for (int i = 0; i < ms.Players.Count; i++)
+        {
+            bounds.Encapsulate(ms.Players[i].transform.position);
+        }
+
+        return (bounds.size.x + bounds.size.y) / ZoomController;
+    }
+
+    public void Move()
+    {
+        Vector3 position = GetCenter() + Offset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, position, ref Change, Speed);
+    }
+
+    Vector3 GetCenter()
+    {
+        MassSpawner ms = MassSpawner.ins;
+
+        Bounds bounds = new Bounds(ms.Players[0].transform.position, Vector3.zero);
+        for (int i = 0; i < ms.Players.Count; i++)
+        {
+            bounds.Encapsulate(ms.Players[i].transform.position);
+        }
+
+        return bounds.center;
     }
 }
