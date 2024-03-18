0746d41d2088bbb892605da6a0d6d086ceac878d
John Yeo <johnyeocx@gmail.com>
2024-03-18 09:26:45 +0000

    commented out code in Virus.cs

--- game/Assets/scripts/Virus.cs
+++ game/Assets/scripts/Virus.cs
@@ -3,42 +3,42 @@ using System.Collections.Generic;
 
 public class Virus : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            GameObject player = other.gameObject;
-            player.transform.localScale += new Vector3(5f, 5f, 5f);
+    // private void OnTriggerEnter2D(Collider2D other)
+    // {
+    //     if (other.CompareTag("Player"))
+    //     {
+    //         GameObject player = other.gameObject;
+    //         player.transform.localScale += new Vector3(5f, 5f, 5f);
 
-            int burstNumber = (int)player.transform.localScale.x;
-            player.transform.localScale /= player.transform.localScale.x;
+    //         int burstNumber = (int)player.transform.localScale.x;
+    //         player.transform.localScale /= player.transform.localScale.x;
 
-            List<GameObject> instantiatedBalls = new List<GameObject>();
-            instantiatedBalls.Add(player);
+    //         List<GameObject> instantiatedBalls = new List<GameObject>();
+    //         instantiatedBalls.Add(player);
 
-            for (int i = 0; i < burstNumber; i++)
-            {
-                GameObject ball = Instantiate(player, player.transform.position, Quaternion.identity);
-                instantiatedBalls.Add(ball);
-            }
+    //         for (int i = 0; i < burstNumber; i++)
+    //         {
+    //             GameObject ball = Instantiate(player, player.transform.position, Quaternion.identity);
+    //             instantiatedBalls.Add(ball);
+    //         }
 
-            float rotation = 360f / burstNumber;
-            int currentRotation = 0;
+    //         float rotation = 360f / burstNumber;
+    //         int currentRotation = 0;
 
-            foreach (GameObject b in instantiatedBalls)
-            {
-                currentRotation += 1;
-                b.transform.rotation = Quaternion.Euler(0, 0, rotation * currentRotation);
-                b.GetComponent<Collider2D>().isTrigger = false;
-                b.GetComponent<CircleCollider2D>().enabled = false;
-                b.GetComponent<PlayerMovement>().LockActions = true;
-                b.GetComponent<SplitForce>().Speed = b.GetComponent<SplitForce>().DefaultSpeed;
-                b.GetComponent<SplitForce>().ApplyForce = true;
-                b.GetComponent<SplitForce>().enabled = true;
-            }
+    //         foreach (GameObject b in instantiatedBalls)
+    //         {
+    //             currentRotation += 1;
+    //             b.transform.rotation = Quaternion.Euler(0, 0, rotation * currentRotation);
+    //             b.GetComponent<Collider2D>().isTrigger = false;
+    //             b.GetComponent<CircleCollider2D>().enabled = false;
+    //             b.GetComponent<PlayerMovement>().LockActions = true;
+    //             b.GetComponent<SplitForce>().Speed = b.GetComponent<SplitForce>().DefaultSpeed;
+    //             b.GetComponent<SplitForce>().ApplyForce = true;
+    //             b.GetComponent<SplitForce>().enabled = true;
+    //         }
 
-            // Destroy the virus
-            Destroy(gameObject);
-        }
-    }
+    //         // Destroy the virus
+    //         Destroy(gameObject);
+    //     }
+    // }
 }
